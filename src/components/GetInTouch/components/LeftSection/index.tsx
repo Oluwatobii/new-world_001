a4c62349f31631c77b1a37ccdb78fc8efcd5373a
Oluwatobii <bellotobi10@gmail.com>
2022-10-16 01:24:46 -0400

    CDE10 - Use placeholder phone number

--- src/components/GetInTouch/components/LeftSection/index.tsx
+++ src/components/GetInTouch/components/LeftSection/index.tsx
@@ -63,7 +63,7 @@ export default function LeftSection() {
           <CustomIcon Icon={<GoLocation size={'45px'} />} title="My Location" text="Ontario, Canada" />
           <CustomIcon Icon={<AiOutlineMail size={'45px'} />} title="Email Address" text="tbellodevelopment@gmail.com" />
           <CustomIcon Icon={<TbWorld size={'45px'} />} title="Website" text="https://tbello.dev" />
-          <CustomIcon Icon={<BsTelephone size={'45px'} />} title="Telephone" text="+1(111)-111-1111" />
+          <CustomIcon Icon={<BsTelephone size={'45px'} />} title="Telephone" text="+1 (xxx) xxx-xxxx" />
         </SimpleGrid>
       </Box>
     </Stack>
