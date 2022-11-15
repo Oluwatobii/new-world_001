1ba5be1919a67f69270ec8cd52db03e180b12192
Oluwatobii <bellotobi10@gmail.com>
2022-11-14 21:45:07 -0500

    CDE18 - Integrate Houston API to get data to display on app with fall back option

---
+++ src/components/Stats/stats.ts
@@ -0,0 +1,22 @@
+export const statData = {
+  data: [
+    {
+      id: '1',
+      subject: 'Awesome Projects',
+      stat: '5+',
+      active: true
+    },
+    {
+      id: '2',
+      subject: 'Years Experiences',
+      stat: '2+',
+      active: true
+    },
+    {
+      id: '3',
+      subject: 'Certifications',
+      stat: '3+',
+      active: true
+    }
+  ]
+}
